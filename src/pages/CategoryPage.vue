2b235d980c10b648976eab1bbc9b40a8f2c282a9
Mark Hatherly <mhatherly@mixvoip.com>
2025-08-19 13:18:58 +0200

    Refactor CategoryPage: replace static content with dynamic collection data from API and remove unused example store.

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -1,23 +1,45 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <h4>category</h4>
+    <q-list v-for="item in collection">
+      <q-item-section>
+        <q-item-label>{{ item.title }}</q-item-label>
+        <q-item-label caption lines="2">{{ item.description }}</q-item-label>
+      </q-item-section>
+    </q-list>
   </q-page>
 </template>
 <script setup lang="ts">
 /* ============ PROPS ============ */
+
+import { api } from 'boot/axios';
+import { onMounted, ref } from 'vue';
+
+defineProps<{
+  collectionId?: number;
+}>();
 
 /* ============ EMITS ============ */
 
 /* ========= LOCAL SCOPE ========= */
 
 /* ============= REFS ============ */
-
+const collection = ref({
+  id: 0,
+  name: '',
+  description: '',
+  image: '',
+  slug: '',
+  created_at: '',
+  updated_at: '',
+});
 /* ============ HOOKS ============ */
 
-/* =========== METHODS =========== */
+onMounted(async () => {
+  collection.value = (await api.get('/api/collections/240', { withCredentials: true })).data;
+  console.log('Collection: ', collection.value);
+});
 
+/* =========== METHODS =========== */
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
