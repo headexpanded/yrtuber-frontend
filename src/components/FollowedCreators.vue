5f40aa3163ee82dc9c3e4489f139afc4e4dc328a
Mark Hatherly <mhatherly@mixvoip.com>
2025-08-20 11:18:11 +0200

    Refactor FollowedCreators: replace card loop with reusable CreatorCard component, fetch creators dynamically, and update layout for responsive grid.

--- src/components/FollowedCreators.vue
+++ src/components/FollowedCreators.vue
@@ -1,24 +1,16 @@
 <template>
-<q-card v-for="creator in creators" :key="creator.id" class="q-mb-md">
-  <q-item clickable :to="{ name: 'creator', params: { id: creator.id } }">
-    <q-item-section avatar>
-      <q-avatar>
-        <img :src="creator.avatar_url" alt="Creator Avatar" />
-      </q-avatar>
-    </q-item-section>
-    <q-item-section>
-      <div class="text-h6">{{ creator.profile.username }}</div>
-      <div class="text-subtitle2">{{ creator.profile.bio }}</div>
-    </q-item-section>
-  </q-item>
-</q-card>
+  <div class="row q-col-gutter-x-xs q-col-gutter-y-xl">
+    <div v-for="creator in creators" :key="creator.id" class="col-xs-12 col-sm-6 col-md-4">
+      <CreatorCard :creator="creator" />
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-/* ============ PROPS ============ */
+import { api } from 'boot/axios';
+import CreatorCard from 'components/CreatorCard.vue';
+import { onMounted, ref } from 'vue';
 
-defineProps<{
-  creators: Array,
-}>()
+/* ============ PROPS ============ */
 
 /* ============ EMITS ============ */
 
@@ -26,7 +18,17 @@ defineProps<{
 
 /* ============= REFS ============ */
 
+const creators = ref([])
+
 /* ============ HOOKS ============ */
+
+onMounted(async () => {
+  const response = await api.get('api/follows/following', {
+    withCredentials: true,
+  });
+  console.log("Creators: ", response.data);
+  creators.value = response.data.data;
+});
 
 /* =========== METHODS =========== */
 
