239ec419ee5a77e5e95e3a269ef5ae17bfda1118
Mark Hatherly <mhatherly@mixvoip.com>
2025-08-20 09:47:55 +0200

    Add tabs to FollowingPage: integrate Collections and Creators tabs with dynamic data and improve i18n labels

---
+++ src/components/FollowedCreators.vue
@@ -0,0 +1,37 @@
+<template>
+<q-card v-for="creator in creators" :key="creator.id" class="q-mb-md">
+  <q-item clickable :to="{ name: 'creator', params: { id: creator.id } }">
+    <q-item-section avatar>
+      <q-avatar>
+        <img :src="creator.avatar_url" alt="Creator Avatar" />
+      </q-avatar>
+    </q-item-section>
+    <q-item-section>
+      <div class="text-h6">{{ creator.profile.username }}</div>
+      <div class="text-subtitle2">{{ creator.profile.bio }}</div>
+    </q-item-section>
+  </q-item>
+</q-card>
+</template>
+<script setup lang="ts">
+/* ============ PROPS ============ */
+
+defineProps<{
+  creators: Array,
+}>()
+
+/* ============ EMITS ============ */
+
+/* ========= LOCAL SCOPE ========= */
+
+/* ============= REFS ============ */
+
+/* ============ HOOKS ============ */
+
+/* =========== METHODS =========== */
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/FollowingPage.vue
+++ src/pages/FollowingPage.vue
@@ -1,10 +1,42 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h4>{{ $t('labels.following') }}</h4>
+  <q-page class="q-pa-md">
+    <q-tabs
+      v-model="activeTab"
+      align="justify"
+      active-color="primary"
+      class="text-grey"
+      indicator-color="primary"
+      narrow-indicator
+    >
+      <q-tab name="collections" :label="$t('labels.collections')" />
+      <q-tab name="creators" :label="$t('labels.creators')" />
+    </q-tabs>
+    <q-tab-panels v-model="activeTab" animated>
+      <q-tab-panel name="collections">
+        <div class="row q-col-gutter-x-xs q-col-gutter-y-xl">
+          <div v-for="item in collections" :key="item.id" class="col-xs-12 col-sm-6 col-md-4">
+            <CollectionCard :item="item" />
+          </div>
+        </div>
+      </q-tab-panel>
+      <q-tab-panel name="creators">
+        <FollowedCreators :creators="creators" />
+      </q-tab-panel>
+    </q-tab-panels>
   </q-page>
 </template>
 <script setup lang="ts">
+import { api } from 'boot/axios';
+import CollectionCard from 'components/CollectionCard.vue';
+import FollowedCreators from 'components/FollowedCreators.vue';
+import type { Collection } from 'src/types/Collection';
+import { onMounted, ref } from 'vue';
+
 /* ============ PROPS ============ */
+
+defineProps<{
+  collectionId?: number;
+}>();
 
 /* ============ EMITS ============ */
 
@@ -12,12 +44,33 @@
 
 /* ============= REFS ============ */
 
+const activeTab = ref('collections');
+const collections = ref<Collection[]>([]);
+const creators = ref([])
 /* ============ HOOKS ============ */
 
+onMounted(async () => {
+  const response = await api.get<Collection[]>('/api/users/500/collections', {
+    withCredentials: true,
+  });
+  console.log('Collection: ', response);
+  collections.value = response.data.data;
+
+  const test = await api.get('api/follows/following', {
+    withCredentials: true,
+  });
+  console.log("Creators: ", test);
+  creators.value = test.data.data;
+});
+
 /* =========== METHODS =========== */
+
 
 </script>
 
 <style scoped lang="scss">
-
+//.q-card {
+//  width: 100%;
+//  max-width: 400px;
+//}
 </style>
