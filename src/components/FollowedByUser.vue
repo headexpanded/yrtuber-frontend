0c688a549161ee482fa3931296e9ec7f21dc4d1a
Mark Hatherly <mhatherly@mixvoip.com>
2025-08-20 11:04:57 +0200

    Refactor FollowingPage: extract tabs and panels into new FollowedByUser component for improved reusability and simplicity.

---
+++ src/components/FollowedByUser.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-tabs
+    v-model="activeTab"
+    align="justify"
+    active-color="primary"
+    class="text-grey"
+    indicator-color="primary"
+    narrow-indicator
+  >
+    <q-tab name="collections" :label="$t('labels.collections')" />
+    <q-tab name="creators" :label="$t('labels.creators')" />
+  </q-tabs>
+  <q-tab-panels v-model="activeTab" animated>
+    <q-tab-panel name="collections">
+        <FollowedCollections />
+    </q-tab-panel>
+    <q-tab-panel name="creators">
+      <div class="row q-col-gutter-x-xs q-col-gutter-y-xl">
+        <FollowedCreators />
+      </div>
+    </q-tab-panel>
+  </q-tab-panels>
+</template>
+<script setup lang="ts">
+import FollowedCollections from 'components/FollowedCollections.vue';
+import FollowedCreators from 'components/FollowedCreators.vue';
+import { ref } from 'vue';
+
+/* ============ PROPS ============ */
+
+/* ============ EMITS ============ */
+
+/* ========= LOCAL SCOPE ========= */
+
+/* ============= REFS ============ */
+
+const activeTab = ref('collections');
+
+/* ============ HOOKS ============ */
+
+
+
+/* =========== METHODS =========== */
+
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/FollowingPage.vue
+++ src/pages/FollowingPage.vue
@@ -1,42 +1,13 @@
 <template>
-  <q-page class="q-pa-md">
-    <q-tabs
-      v-model="activeTab"
-      align="justify"
-      active-color="primary"
-      class="text-grey"
-      indicator-color="primary"
-      narrow-indicator
-    >
-      <q-tab name="collections" :label="$t('labels.collections')" />
-      <q-tab name="creators" :label="$t('labels.creators')" />
-    </q-tabs>
-    <q-tab-panels v-model="activeTab" animated>
-      <q-tab-panel name="collections">
-        <div class="row q-col-gutter-x-xs q-col-gutter-y-xl">
-          <div v-for="item in collections" :key="item.id" class="col-xs-12 col-sm-6 col-md-4">
-            <CollectionCard :item="item" />
-          </div>
-        </div>
-      </q-tab-panel>
-      <q-tab-panel name="creators">
-        <FollowedCreators :creators="creators" />
-      </q-tab-panel>
-    </q-tab-panels>
+  <q-page padding>
+    <FollowedByUser />
   </q-page>
 </template>
 <script setup lang="ts">
-import { api } from 'boot/axios';
-import CollectionCard from 'components/CollectionCard.vue';
-import FollowedCreators from 'components/FollowedCreators.vue';
-import type { Collection } from 'src/types/Collection';
-import { onMounted, ref } from 'vue';
+import FollowedByUser from 'components/FollowedByUser.vue';
 
 /* ============ PROPS ============ */
 
-defineProps<{
-  collectionId?: number;
-}>();
 
 /* ============ EMITS ============ */
 
@@ -44,33 +15,12 @@ defineProps<{
 
 /* ============= REFS ============ */
 
-const activeTab = ref('collections');
-const collections = ref<Collection[]>([]);
-const creators = ref([])
 /* ============ HOOKS ============ */
 
-onMounted(async () => {
-  const response = await api.get<Collection[]>('/api/users/500/collections', {
-    withCredentials: true,
-  });
-  console.log('Collection: ', response);
-  collections.value = response.data.data;
-
-  const test = await api.get('api/follows/following', {
-    withCredentials: true,
-  });
-  console.log("Creators: ", test);
-  creators.value = test.data.data;
-});
 
 /* =========== METHODS =========== */
 
 
 </script>
 
-<style scoped lang="scss">
-//.q-card {
-//  width: 100%;
-//  max-width: 400px;
-//}
-</style>
+<style scoped lang="scss"></style>
